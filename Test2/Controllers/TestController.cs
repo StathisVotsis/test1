d7881f1332271baf0bf0020d52890b4d00b095a1
Stathis Votsis <stathisvotsis@gmail.com>
2017-03-05 18:33:45 +0200

    First commit

---
+++ Test2/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Test2.Controllers
+{
+    [RoutePrefix("api/Test")]
+    public class TestController : ApiController
+    {
+        [Route("")]
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new {el = 1, el2 = 2 });
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public HttpResponseMessage GetById(int id)
+        {
+            id = id + 2;
+            return Request.CreateResponse(HttpStatusCode.OK, new { Id = id });
+        }
+    }
+}
